d645424abce9c1609b33401610abfea267b7be09
NohaHesham <Noha-Hesham@users.noreply.github.com>
2021-07-06 19:45:36 +0200

    create a running local server on port:6060

--- server.js
+++ server.js
@@ -2,18 +2,28 @@
 projectData = {};
 
 // Require Express to run server and routes
-
-// Start up an instance of app
+const express = require('express');
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors= require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Start up an instance of app
+const port = 6060;
+const server = app.listen(port, listening);
+function listening() {
+    console.log('server runing');
+    console.log(`running on localhost: ${port}`);
+};
 
 // Setup Server
